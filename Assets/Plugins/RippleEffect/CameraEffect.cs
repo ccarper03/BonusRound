d30d6fa63753013f99bcd8c272e87a3c38748834
Charles Carper <ccarper03@gmail.com>
2021-05-23 21:44:10 -0400

    Finally added the distortion shader!!!!

---
+++ Assets/Plugins/RippleEffect/CameraEffect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraEffect : MonoBehaviour
+{
+	[SerializeField]
+	protected Material effectMat = default;
+
+	protected virtual void Awake()
+	{
+		StartCoroutine(Animate());
+	}
+
+	protected virtual void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		Graphics.Blit(src, dest, effectMat);
+	}
+	IEnumerator Animate()
+    {
+        while (true)
+        {
+			yield return new WaitForSeconds(.5f);
+			effectMat.SetFloat("_DriftX", -2);
+			effectMat.SetFloat("_DriftY", -2);
+
+			yield return new WaitForSeconds(.5f);
+			effectMat.SetFloat("_DriftX", 2);
+			effectMat.SetFloat("_DriftY", 2);
+		}
+		
+	}
+}
---
+++ Assets/Plugins/RippleEffect/CameraEffectRipple.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraEffectRipple : CameraEffect
+{
+	private const string PropertyName = "_Magnitude";
+
+	[SerializeField]
+	[Range(0, .1f)]
+	private float startMagnitude = .03f;
+
+	[SerializeField]
+	private float fallOffPerSecond = .015f;
+
+	private IEnumerator rippleOut;
+	private IEnumerator rippleIn;
+
+	protected override void Awake()
+	{
+		base.Awake();
+		ResetMagnitude();
+#if UNITY_EDITOR
+		//EditorEventManager.ExitingPlayMode += OnExitingPlayMode;
+#endif
+	}
+
+#if UNITY_EDITOR
+
+	private void OnDestroy()
+	{
+		//EditorEventManager.ExitingPlayMode -= OnExitingPlayMode;
+	}
+
+	private void OnExitingPlayMode()
+	{
+		if (rippleOut != null)
+		{
+			StopCoroutine(rippleOut);
+		}
+		if (rippleIn != null)
+		{
+			StopCoroutine(rippleIn);
+		}
+		ResetMagnitude();
+	}
+
+#endif
+
+	public void StartRippleOut()
+	{
+		rippleOut = RippleOut();
+		StartCoroutine(rippleOut);
+	}
+
+	public void StartRippleIn(bool thenRippleOut = false)
+	{
+		rippleIn = RippleIn(thenRippleOut);
+		StartCoroutine(rippleIn);
+	}
+
+	private IEnumerator RippleOut()
+	{
+		float currentMagnitude = effectMat.GetFloat(PropertyName);
+		while (currentMagnitude > 0)
+		{
+			yield return new WaitForEndOfFrame();
+			effectMat.SetFloat(PropertyName, currentMagnitude -= (fallOffPerSecond * Time.deltaTime));
+		}
+		enabled = false;
+		ResetMagnitude();
+	}
+
+	private IEnumerator RippleIn(bool thenRippleOut)
+	{
+		enabled = true;
+		float currentMagnitude = effectMat.GetFloat(PropertyName);
+		while (currentMagnitude < startMagnitude)
+		{
+			yield return new WaitForEndOfFrame();
+			effectMat.SetFloat(PropertyName, currentMagnitude += (fallOffPerSecond * Time.deltaTime));
+		}
+		ResetMagnitude();
+		if (thenRippleOut)
+		{
+			StartRippleOut();
+		}
+	}
+
+	private void ResetMagnitude()
+	{
+		effectMat.SetFloat(PropertyName, startMagnitude);
+	}
+}
